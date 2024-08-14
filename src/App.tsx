fd146d9d31f40fcc529299d267641b43450ff813
joao <pedro_venc12@hotmail.com>
2024-08-14 15:58:11 -0300

    psubindo

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import { useEffect } from 'react'
+// import { useState } from 'react'
+import api from './services/api'
+
+function App() {
+  // const [products, setProducts] = useState([])
+
+  // async function getProducts(){
+  //   const response = await api.get('/produtos')
+  //   console.log(response);
+  // }
+
+  // useEffect(() => {
+  //   getProducts()
+  // }, [])
+
+
+
+  return (
+    <>
+      <h1>ola mundo</h1>
+    </>
+  )
+}
+
+export default App
+
+
+
